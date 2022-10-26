c50a106d061726d9c4897af39a0741986ee1044c
SAIRUS VARMA <sairusvarma201234@gmail.com>
2022-10-26 19:20:25 +0530

    initial commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,25 @@
+import React  from "react";
+
+
+const About = () => {
+    return (
+        <div className="container">
+            <div className="py-4">
+                <h1>About Page</h1>
+                <p className="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Neque, velit distinctio eius architecto voluptatum illo quisquam sed unde.
+                 Eum quae vitae, magnam deserunt tenetur nostrum ut voluptate tempora eos esse.</p>
+
+                 <p className="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Neque, velit distinctio eius architecto voluptatum illo quisquam sed unde.
+                 Eum quae vitae, magnam deserunt tenetur nostrum ut voluptate tempora eos esse.</p>
+
+                 <p className="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Neque, velit distinctio eius architecto voluptatum illo quisquam sed unde.
+                 Eum quae vitae, magnam deserunt tenetur nostrum ut voluptate tempora eos esse.</p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
